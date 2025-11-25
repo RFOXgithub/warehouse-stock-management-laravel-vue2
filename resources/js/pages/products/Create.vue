8f9be360eddf59e84cebaf791c0280f5220365ee
RFOXgithub <rivalmanti399@gmail.com>
2025-11-25 14:53:19 +0700

    Update

---
+++ resources/js/pages/products/Create.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import Input from '@/components/ui/input/Input.vue';
+import Label from '@/components/ui/label/Label.vue';
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Create a Product',
+        href: '/products/create',
+    },
+];
+</script>
+
+<template>
+    <Head title="Create a Product" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="w-8/12 space-y-4 p-4">
+            <Label for="kode-barang">Kode Barang</Label>
+            <Input type="text" placeholder="Kode Barang" />
+            <Label for="nama-barang">Nama Barang</Label>
+            <Input type="text" placeholder="Nama Barang" />
+            <Label for="satuan">Satuan</Label>
+            <Select>
+                <SelectTrigger class="w-[180px]">
+                    <SelectValue placeholder="Select a fruit" />
+                </SelectTrigger>
+                <SelectContent>
+                    <SelectGroup>
+                        <SelectLabel>Fruits</SelectLabel>
+                        <SelectItem value="apple"> Apple </SelectItem>
+                        <SelectItem value="banana"> Banana </SelectItem>
+                        <SelectItem value="blueberry"> Blueberry </SelectItem>
+                        <SelectItem value="grapes"> Grapes </SelectItem>
+                        <SelectItem value="pineapple"> Pineapple </SelectItem>
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
+            <Label for="stock-awal">Stock Awal</Label>
+            <Input type="number" placeholder="Stock Awal" />
+        </div>
+    </AppLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -14,4 +15,9 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+});
+
+require __DIR__ . '/settings.php';
